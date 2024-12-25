e5953c104e8c6fd7a2a1f8fab39591002e6b53bf
Frodddo <15543168578@qq.com>
2024-12-25 13:16:25 +0800

    2

---
+++ petspringdemo/src/main/java/org/example/petspringdemo/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package org.example.petspringdemo.service.impl;
+
+
+import org.example.petspringdemo.entity.UserInfo;
+import org.example.petspringdemo.mapper.UserMapper;
+import org.example.petspringdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public void insertUser(UserInfo userInfo)
+    {
+        userMapper.insertUser(userInfo);
+    }
+
+    @Override
+    public boolean checkUserName(String userName)
+    {
+        return  userMapper.checkUserName(userName);
+    }
+
+    @Override
+    public int verify(String userName,String password)
+    {
+        return userMapper.verify(userName,password);
+    }
+
+    @Override
+    public String getIdentity(String username)
+    {
+        return userMapper.getIdentity(username);
+    }
+}
---
+++ petspringdemo/src/test/java/org/example/petspringdemo/PetspringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.petspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetspringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
